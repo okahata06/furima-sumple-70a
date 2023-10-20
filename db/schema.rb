18f006ff021ecbca6de8aa4c7372108754f8b0a4
岡畑藍 <airuo0603@gmail.com>
2023-10-20 18:09:00 +0900

    なんか消した

--- db/schema.rb
+++ db/schema.rb
@@ -24,15 +24,6 @@ ActiveRecord::Schema.define(version: 2020_03_19_034504) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
-  end
-
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body", null: false
     t.integer "product_id", null: false
